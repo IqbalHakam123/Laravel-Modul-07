212133be40beae9251b16a1235bfe2654221996c
Tabina Shafa <tabinashafa@gmail.com>
2023-06-21 21:57:34 +0700

    File tampilan project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Version Control System</title>
+    <title>{{ $pageTitle }}</title>
+    @vite('resources/sass/app.scss')
 </head>
 <body>
-    <h1>Belajar version control system</h1>
+    @include('layouts.nav')
+    @yield('content')
+    @vite('resources/js/app.js')
 </body>
 </html>
